9cd9c5613dccc24de211ca453a09f59a3015bcc7
daniel-dunsin <adejaredaniel12@gmail.com>
2023-07-06 09:22:49 +0100

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,58 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import rateLimiter from 'express-rate-limit';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import { config } from 'dotenv';
+import { errorHandler, notFound } from './utils/errorHandlers';
+
+config();
+
+const app: express.Application = express();
+
+/**
+ * Middlewares
+ */
+// Max of 20 requests per minute
+app.use(
+  rateLimiter({
+    windowMs: 60000,
+    max: 20,
+  })
+);
+// Helps to secure requests by setting headers
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/**
+ * Routes
+ */
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.status(200).json({ message: 'Welcome to realtor API' });
+});
+
+/**
+ * Error Handlers
+ */
+app.use(errorHandler);
+app.all('*', notFound);
+
+/**
+ * Connection
+ */
+
+const port: string | number | undefined = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.DB_URL as string)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ src/utils/errorHandlers.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+
+export class CustomError extends Error {
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.message = message;
+    (this as any).code = statusCode;
+  }
+}
+
+export const errorHandler = (
+  error: Error | CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error instanceof CustomError) {
+    return res.status((error as any).code).json({ error: error.message });
+  }
+
+  return res.status(500).json({ error: `${error.toString()}` });
+};
+
+export const notFound = (req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ error: 'Route does not exist' });
+};
